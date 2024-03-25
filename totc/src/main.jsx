6d1fd99674a4e6ad48fd4b78565a35826478c624
mishlosh <mishlosh@gmail.com>
2024-03-25 17:02:52 -0400

    testing api calls

--- totc/src/main.jsx
+++ totc/src/main.jsx
@@ -4,11 +4,16 @@ import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.css";
 import "./styles.css";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
