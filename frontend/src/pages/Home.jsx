bc695acc96f13b385136a05f395bb05b1897983e
wik37 <98570246+wik37@users.noreply.github.com>
2024-04-25 22:08:35 -0400

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import placesData from './placesData';
+import React, { useState } from 'react';
+import './CaribbeanMap.css';
+
 import trinidadIcon from './Images/Trinidad.png';
-import barbadosIcon from './Images/Barbados.png'; 
+import barbadosIcon from './Images/Barbados.png';
 import jamaicaIcon from './Images/Jamaica.png';
 import cubaIcon from './Images/Cuba.png';
 import drIcon from './Images/DR.png';
@@ -12,91 +10,95 @@ import haitiIcon from './Images/Haiti.png';
 import bahamasIcon from './Images/Bahamas.png';
 import puertoricoIcon from './Images/PuertoRico.png';
 import guyanaIcon from './Images/Guyana.png';
+import logo from './Images/logo.png'; 
 
-// Defines custom marker icons for each city
-const customMarkerIcons = {
-  "Trinidad and Tobago": new L.Icon({
-    iconUrl: trinidadIcon,
-    iconSize: [60, 60],
-    iconAnchor: [20, 40],
-    popupAnchor: [0, -40],
-  }),
-  "Barbados": new L.Icon({
-    iconUrl: barbadosIcon,
-    iconSize: [60, 60],
-    iconAnchor: [20, 40],
-    popupAnchor: [0, -40],
-  }),
-  "Jamaica": new L.Icon({
-    iconUrl: jamaicaIcon,
-    iconSize: [60, 60],
-    iconAnchor: [20, 40],
-    popupAnchor: [0, -40],
-  }),
-   "Cuba": new L.Icon({
-    iconUrl: cubaIcon, 
-    iconSize: [60, 60],
-    iconAnchor: [20, 40],
-    popupAnchor: [0, -40],
-  }),
-   "Dominican Republic": new L.Icon({
-    iconUrl: drIcon, 
-    iconSize: [60, 60],
-    iconAnchor: [20, 40],
-    popupAnchor: [0, -40],
-  }),
-   "Haiti": new L.Icon({
-    iconUrl: haitiIcon, 
-    iconSize: [60, 60],
-    iconAnchor: [20, 40],
-    popupAnchor: [0, -40],
-  }),
-   "Bahamas": new L.Icon({
-    iconUrl: bahamasIcon, 
-    iconSize: [60, 60],
-    iconAnchor: [20, 40],
-    popupAnchor: [0, -40],
-  }),
-   "Puerto Rico": new L.Icon({
-    iconUrl: puertoricoIcon, 
-    iconSize: [60, 60],
-    iconAnchor: [20, 40],
-    popupAnchor: [0, -40],
-  }),
-   "Guyana": new L.Icon({
-    iconUrl: guyanaIcon, 
-    iconSize: [60, 60],
-    iconAnchor: [20, 40],
-    popupAnchor: [0, -40],
-  }),
-};
+const CaribbeanMap = () => {
+    const [selectedCountry, setSelectedCountry] = useState('');
+    const [countryInfo, setCountryInfo] = useState('');
 
-const MapComponent = () => {
-  const mapTilerApiKey = 'A8tTZzhY1wOV3ufAtDq2';
+    const icons = [
+        {
+            name: "Trinidad and Tobago",
+            icon: trinidadIcon,
+            style: { top: '78%', left: '85%' },
+            info: "Port of Spain is the capital city of Trinidad and Tobago. The national dish of Trinidad and Tobago is called 'roti,' a delicious flatbread typically filled with curried meat, vegetables, or chickpeas."
+        },
+        {
+            name: "Barbados",
+            icon: barbadosIcon,
+            style: { top: '60%', left: '89%' },
+            info: "Bridgetown is the capital city of Barbados. The national dish of Barbados is 'cou-cou and flying fish,' a savory dish made from cornmeal and okra, served with seasoned flying fish."
+        },
+        {
+            name: "Jamaica",
+            icon: jamaicaIcon,
+            style: { top: '41%', left: '35%' },
+            info: "Kingston is the capital city of Jamaica. The national dish of Jamaica is 'ackee and saltfish,' a flavorful dish made from salted codfish and ackee fruit, typically served with fried plantains and dumplings."
+        },
+        {
+            name: "Cuba",
+            icon: cubaIcon,
+            style: { top: '26%', left: '37%' },
+            info: "Havana is the capital city of Cuba. The national dish of Cuba is 'ropa vieja,' a hearty stew made from shredded beef, tomatoes, onions, and peppers, often served with rice and black beans."
+        },
+        {
+            name: "Dominican Republic",
+            icon: drIcon,
+            style: { top: '33%', left: '55%' },
+            info: "Santo Domingo is the capital city of the Dominican Republic. The national dish of the Dominican Republic is 'mangu,' a mashed plantain dish served with sautéed onions, cheese, and fried eggs."
+        },
+        {
+            name: "Haiti",
+            icon: haitiIcon,
+            style: { top: '35%', left: '49%' },
+            info: "Port-au-Prince is the capital city of Haiti. The national dish of Haiti is 'joumou,' a pumpkin soup traditionally served on Independence Day, January 1st."
+        },
+        {
+            name: "Bahamas",
+            icon: bahamasIcon,
+            style: { top: '3%', left: '30%' },
+            info: "Nassau is the capital city of the Bahamas. The national dish of the Bahamas is 'conch salad,' a refreshing salad made from conch meat, peppers, onions, and citrus juice."
+        },
+        {
+            name: "Puerto Rico",
+            icon: puertoricoIcon,
+            style: { top: '37%', left: '65%' },
+            info: "San Juan is the capital city of Puerto Rico. The national dish of Puerto Rico is 'mofongo,' a savory dish made from mashed fried plantains, garlic, and pork cracklings, often served with a garlic or tomato-based sauce."
+        },
+        {
+            name: "Guyana",
+            icon: guyanaIcon,
+            style: { top: '90%', left: '90%' },
+            info: "Georgetown is the capital city of Guyana. The national dish of Guyana is 'pepperpot,' a rich and spicy meat stew made with cassareep (a thick, black sauce made from cassava), spices, and various meats such as beef, pork, or chicken."
+        },
+    ];
 
-  return (
-    <MapContainer center={[15, -70]} zoom={6} style={{ height: '100vh', width: '100%' }}>
-      {/* Use MapTiler API for custom map tiles */}
-      <TileLayer
-        url={`https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=${mapTilerApiKey}`}
-        attribution="&copy; MapTiler, &copy; OpenStreetMap contributors"
-        minZoom={5} 
-        maxZoom={10} 
-      />
 
-      {/* Render markers with custom marker icons and styled popups */}
-      {placesData.map(place => (
-        <Marker position={place.coordinates} icon={customMarkerIcons[place.name]} key={place.name}>
-          <Popup>
-            <div style={{ textAlign: 'center' }}>
-              <h2>{place.name}</h2>
-              <p>{place.foodInfo}</p>
+    const selectCountry = (country, info) => {
+        setSelectedCountry(country);
+        setCountryInfo(info);
+    };
+
+    return (
+        <div className="map-container">
+            <div className="logo-container">
+                <img src={logo} alt="Logo" className="logo" />
             </div>
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
-  );
+            <div className="info-box">
+                {selectedCountry && <p>{countryInfo}</p>}
+            </div>
+            {icons.map((country, index) => (
+                <div key={index} style={{ position: 'absolute', top: country.style.top, left: country.style.left }}>
+                    <img
+                        src={country.icon}
+                        alt={country.name}
+                        className="icon"
+                        onClick={() => selectCountry(country.name, country.info)}
+                    />
+                </div>
+            ))}
+        </div>
+    );
 };
 
-export default MapComponent;
+export default CaribbeanMap;
